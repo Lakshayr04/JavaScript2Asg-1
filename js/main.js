5ecee295418c04d2be8b9e1d0bd7f2e74e39e0ff
Lakshayr04 <124931054+Lakshayr04@users.noreply.github.com>
2023-09-29 16:02:35 -0500

    Added implentation for main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// WEB303 Assignment 2
+$(document).ready(function() {
+    $('#prospect, #convert, #retain').on('click', function(e) {
+    var xhr=new XMLHttpRequest();
+    if(e.target.id==="prospect")  {
+        xhr.open('GET','prospect.html',true)
+    }else if (e.target.id==="convert"){
+        xhr.open('GET','convert.html',true)
+
+    }else{
+        xhr.open('GET','retain.html',true)
+    }
+        xhr.onload = function(){
+            if(this.status===200)   {
+                let element=document.getElementById("content")
+                element.innerHTML=xhr.responseText
+            }
+        }
+        $("#content").slideDown()
+        xhr.send()
+    })
+})
+
